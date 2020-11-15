b72a2703a01ccf160d64f6cad6571422a0b73460
Nicola Goderis <64043286+devnicg@users.noreply.github.com>
2020-11-15 01:27:42 +0100

    adding an initial CsvParser

---
+++ csvparser.py
@@ -0,0 +1,106 @@
+
+
+class CsvParser():
+    def __init__(self, csv_file, seperator):
+        """Initialize the CsvParser.
+
+        Args:
+            csv_file (String): path to a CSV file.
+            sep (String): Seperator used in given CSV file.
+        """
+        self.csv_file = csv_file
+        self.seperator = seperator
+        self.headers = self.__getHeaders()
+        self.data = self.__getData()
+        self.dataDict = self.__convertDataToDict()
+        
+
+    def __stripArray(self, inputArray):
+        """Strip the array of stringtags (' or ").
+
+        Args:
+            inputArray (List): Array of strings with strings that still have stringtags.
+
+        Returns:
+            [List]: strings stripped of stringtags (' or ").
+        """
+        arr = []
+        for i in inputArray:
+            if i.startswith("\"") and i.endswith("\""):
+                i = i.strip("\"")
+            elif i.startswith("\'") and i.endswith("\'"):
+                i = i.strip("\'")
+            if "\n" in i:
+                i = i.strip("\n")
+            arr.append(i)
+        return arr
+
+    def __getHeaders(self):
+        """Gets all the headers from the given csv files.
+
+        Returns:
+            [List]: all the headers of the CSV file.
+        """
+        with open(self.csv_file, 'r') as c:
+            headers = c.readline().split(self.seperator)
+            stripped_headers = self.__stripArray(headers)
+            return stripped_headers
+    
+    def __getData(self):
+        """Gets all the data from the given csv file.
+
+        Returns:
+            [List]: Each row of the CSV file converted to a list.
+        """
+        data = []
+        with open(self.csv_file, 'r') as c:
+            lines = c.readlines()
+            for line in lines:
+                if line == lines[0]:
+                    continue
+                else:
+                    stripped_data = self.__stripArray(line.split(self.seperator))
+                    data.append(stripped_data)
+        return data
+
+    def __convertDataToDict(self):
+        """Converts all entries to dictionaries with the headers as keys.
+
+        Returns:
+            [Dict]: dictionaries of all the CSV entries.
+        """
+        lineobjects = {}
+        i = 0
+        while i < len(self.data):
+            for line in self.data:
+                i2 = 0
+                lineobject = {}
+                while i2 < len(self.headers):
+                    lineobject[self.headers[i2]] = line[i2]
+                    i2 += 1
+                lineobjects[i] = lineobject
+                i += 1
+
+        return lineobjects
+
+
+if __name__ == '__main__':
+    from testcsv import TestCsvs
+    import os
+    testfolder = '2018-census-totals-by-topic-national-highlights-csv'
+    myTestCsvs = TestCsvs(testfolder)
+    mycsv_list = myTestCsvs.csvList
+
+    def log(toLog):
+        print("\n")
+        print(toLog)
+
+    
+    for csv in mycsv_list:
+        try:
+            if csv.endswith(".csv"):
+                parsed = CsvParser(os.path.join(testfolder, csv), ",")
+                log(csv)
+                log(parsed.dataDict)
+        except:
+            print(f"Failed to parse {csv}")
